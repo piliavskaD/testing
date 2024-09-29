471a00381f8b91c4360a6c314af6fc01b243f612
piliavskaD <pilavskadasa@gmail.com>
2024-09-29 23:16:26 +0300

    ready

--- xxxx.py
+++ xxxx.py
@@ -1,15 +1,8 @@
-import math 
+def ppl(a, b):
 
-def ff(x):
-    if x >= 1:
-        f = 7 * math.log(x, math.e) + math.log(x, 7) + math.log(x, 10)
-        return f
-    elif -10.3 < x < 1:
-        f = math.sin(x) - math.cos(x + 2 + math.pi / 7)
-        return f
-    elif x <= -10.3:
-        f = 2.24 * math.pow(math.e, 0.5 * x + 0.1) * math.pow(2, 0.3 * x)
-        return f
-    
-x = int(input("x = "))
-print(ff(x))
+    third1 = int(str(a)[2])
+    third2 = int(str(b)[2])
+    return third1 + third2, third2 * third1
+a = int(input("a = "))
+b = int(input("b = "))
+print(ppl(a, b))
