4b84a126374af1eeb907cc8f5db9fe5d10934e12
piliavskaD <pilavskadasa@gmail.com>
2024-09-30 13:10:19 +0300

    new changes

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-a = 10
-b = 6
-print(a + b)
+def ff(a):
+    r = int(str(a)[0]) - int(str(a)[2])
+    return r
+
+while True:
+    a = int(input("a = "))
+    if len(str(a)) == 3:
+      break
+    else:
+      print("repeat")
+
+#dd = ff(a)
+print(ff(a))
